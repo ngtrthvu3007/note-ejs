3bc0ed878a382626a5e09a6b6699fbc23cab6b91
VutheNnt <thevu@advn.vn>
2024-04-14 02:57:46 +0700

    setup env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
 const MongoStore = require("connect-mongo");
-const indexRoutes  = require("./routes/index.routes");
+const indexRoutes = require("./routes/index.routes");
 // const connectDB = require('./server/config/db');
 
 const app = express();
@@ -14,15 +14,14 @@ const port = 4000 || process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use(methodOverride("_method"));
-app.use(express.static("src/public"))
+app.use(express.static("src/public"));
 app.use(expressLayouts);
-app.set('views', 'src/views');
+app.set("views", "src/views");
 app.set("view engine", "ejs");
-app.set('layout', 'layouts/main.layouts.ejs')
+app.set("layout", "layouts/main.layouts.ejs");
 
-app.use("/",  indexRoutes);
+app.use("/", indexRoutes);
 
 app.get("*", function (req, res) {
   res.status(404).render("404");
@@ -31,3 +30,5 @@ app.get("*", function (req, res) {
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+console.log(process.env.NODE_ENV);
